bc2c44d381e804500d33b1a70c19254df4b9be8f
Charles 'Buck' Krasic <krasic@users.sourceforge.net>
2001-10-28 03:23:56 +0000

    Apply patch #475394: "Fix for mmx_ok SEGV with broken gcc-fPIC" submitted by David Daney.

--- libdv/mmx.h
+++ libdv/mmx.h
@@ -72,6 +72,9 @@ mm_support(void)
 	__asm__ __volatile__ (
 		/* See if CPUID instruction is supported ... */
 		/* ... Get copies of EFLAGS into eax and ecx */
+                "pushl %%ebx\n\t"
+                "pushl %%ecx\n\t"
+                "pushl %%edx\n\t"
 		"pushf\n\t"
 		"popl %%eax\n\t"
 		"movl %%eax, %%ecx\n\t"
@@ -207,9 +210,11 @@ mm_support(void)
 		"movl $0, %0\n\n\t"
 
 		"Return:\n\t"
+                "popl %%edx\n\t"
+                "popl %%ecx\n\t"
+                "popl %%ebx\n\t"
 		: "=a" (rval)
 		: /* no input */
-		: "ebx", "ecx", "edx"
 	);
 
 	/* Return */
