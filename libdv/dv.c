9ee56f59094557978fb452379bbfd43cc3f10d6a
Charles 'Buck' Krasic <krasic@users.sourceforge.net>
2003-10-20 19:46:48 +0000

    Change license to LGPL, with permission of all the authors.

--- libdv/dv.c
+++ libdv/dv.c
@@ -7,17 +7,17 @@
  *  codec.
  *
  *  libdv is free software; you can redistribute it and/or modify it
- *  under the terms of the GNU General Public License as published by
- *  the Free Software Foundation; either version 2, or (at your
+ *  under the terms of the GNU Lesser Public License as published by
+ *  the Free Software Foundation; either version 2.1, or (at your
  *  option) any later version.
  *   
  *  libdv is distributed in the hope that it will be useful, but
  *  WITHOUT ANY WARRANTY; without even the implied warranty of
  *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- *  General Public License for more details.
+ *  Lesser Public License for more details.
  *   
- *  You should have received a copy of the GNU General Public License
- *  along with GNU Make; see the file COPYING.  If not, write to
+ *  You should have received a copy of the GNU Lesser Public License
+ *  along with libdv; see the file COPYING.  If not, write to
  *  the Free Software Foundation, 675 Mass Ave, Cambridge, MA 02139, USA. 
  *
  *  The libdv homepage is http://libdv.sourceforge.net/.  
@@ -621,6 +621,7 @@ dv_report_video_error (dv_decoder_t *dv, uint8_t *data)
      */
     if ((data [i] & 0xe0) == 0x80) {
       error_code = data [i + 3] >> 4;
+#if 0
       if (error_code) {
         dv_get_timestamp (dv, err_msg1);
         dv_get_recording_datetime (dv, err_msg2);
@@ -635,6 +636,7 @@ dv_report_video_error (dv_decoder_t *dv, uint8_t *data)
                  data [i + 2],
                  data [i + 3]);
       }
+#endif
     }
   }
 }
